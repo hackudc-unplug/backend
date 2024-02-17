c820a0d7e4a529a970b0b7e7869d4a9618c9a077
Oriol Agost Batalla <gpg18@agost.info>
2024-02-17 18:25:15 +0100

    feat: create static directories when serving

--- localhost.py
+++ localhost.py
@@ -1,4 +1,15 @@
+import os
 import uvicorn
 
+from services.bill.bil_imp import EXTRACT_DIR
+from services.bill.bill_service import BILL_DIR
+from services.image_service import IMAGE_UPLOAD_DIR
+
 if __name__ == "__main__":
+    if not os.path.exists(EXTRACT_DIR):
+        os.makedirs(EXTRACT_DIR)
+    if not os.path.exists(IMAGE_UPLOAD_DIR):
+        os.makedirs(IMAGE_UPLOAD_DIR)
+    if not os.path.exists(BILL_DIR):
+        os.makedirs(BILL_DIR)
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
--- serve.py
+++ serve.py
@@ -1,4 +1,15 @@
 import uvicorn
+import os
+
+from services.bill.bil_imp import EXTRACT_DIR
+from services.image_service import IMAGE_UPLOAD_DIR
+from services.bill.bill_service import BILL_DIR
 
 if __name__ == "__main__":
+    if not os.path.exists(EXTRACT_DIR):
+        os.makedirs(EXTRACT_DIR)
+    if not os.path.exists(IMAGE_UPLOAD_DIR):
+        os.makedirs(IMAGE_UPLOAD_DIR)
+    if not os.path.exists(BILL_DIR):
+        os.makedirs(BILL_DIR)
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
